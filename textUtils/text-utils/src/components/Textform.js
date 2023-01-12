0a93bfe6a28a7f55e3cdc156b465c674d3c4591f
errunlee <32530613+errunlee@users.noreply.github.com>
2023-01-12 14:51:55 +0545

    init

--- textUtils/text-utils/src/components/Textform.js
+++ textUtils/text-utils/src/components/Textform.js
@@ -1,61 +1,71 @@
-import React, {useState}from 'react'
-
-export default function Textform() {
-    let [text,setText]=useState("");
-    let [btnText,setBtnText]=useState("Copy text")
-    const handleUpClick=()=>{
-        let newText=text.toUpperCase()
+import React, { useState } from 'react'
+import Alert from './Alert';
+export default function Textform(props) {
+    let [text, setText] = useState("");
+    let [btnText, setBtnText] = useState("Copy text")
+    const handleUpClick = () => {
+        let newText = text.toUpperCase()
         setText(newText);
         setBtnText('Copy text')
+        props.showAlert('Converted to Uppercase')
     }
-    const handleChange=(e)=>{
+    const handleChange = (e) => {
         setText(e.target.value);
-    }
-    const handleLowClick=()=>{
-        let newText=text.toLowerCase()
-        setText(newText);
         setBtnText('Copy text')
     }
-    const handleClear=()=>{
-        let newText=''
+    const handleLowClick = () => {
+        let newText = text.toLowerCase()
         setText(newText);
         setBtnText('Copy text')
+        props.showAlert('Converted to Lowercase')
     }
-    const handleEmailClick=()=>{
+    const handleClear = () => {
+        let newText = ''
+        setText(newText);
+        setBtnText('Copy text')
+        props.showAlert('Cleared Successfully')
+    }
+    const handleEmailClick = () => {
         let newText = text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-        if(newText){
+        if (newText) {
             setText(newText.join('\n'))
         }
-        else{
+        else {
             setText('')
         }
-        setBtnText('Copied to clipboard')
-
+        setBtnText('Copy text')
+        props.showAlert('Successfully filtered email/s')
     }
-    const handleCopy=()=>{
+    const handleCopy = () => {
         navigator.clipboard.writeText(text);
         setBtnText('Copied to clipboard')
+        props.showAlert('Copied text successfully')
     }
-  return (
-   <>
-    <div className='container'>
-        <h1 className='display-6 text-center'>Enter the text to analyze below.</h1>
-      <div className="my-3">
-  <textarea className="form-control mb-2" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleChange}></textarea>
-  <button className="btn btn-secondary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
-  <button className="btn btn-secondary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
-  <button className="btn btn-secondary mx-1" onClick={handleEmailClick}>Purify Emails</button>
-  <button className="btn btn-secondary mx-1" onClick={handleClear}>Clear Screen</button>
-  <button id='copy' className="btn btn-secondary mx-1" onClick={handleCopy} >{btnText}</button>
-</div>
-    </div>
-    <div className="container my-3">
-        <h1 className='lead'>Your text summary</h1>
-        <p> <span className='display-6'>{text.split(" ").length-1}</span> words, <span className='display-6'>{text.length}</span>characters</p>
-        <p>Average time to read the above text : {text.split(" ").length/125} minutes</p>
-        <h2>Preview:</h2>
-        <p>{text}</p>
-    </div>
-    </>
-  )
+    const darkText = { color: 'black' }
+    const lightText = { color: 'white' }
+
+    return (
+        <>
+            <div className='container'  >
+                <h1 className='display-6 text-center' style={props.mode === 'dark' ? lightText : darkText}>Enter the text to analyze below.</h1>
+                <div className="my-3 position-relative">
+                    {props.alertMsg && <Alert message={props.alertMsg} />}
+                    <textarea style={props.mode === 'dark' ? { backgroundColor: 'black', color: 'white' } : { backgroundColor: 'white', color: 'black' }} className={`form-control mb-2 bg-${props.mode} text-${props.mode==='light'?'dark':'light'}`}id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleChange}></textarea>
+                    <button className="btn btn-secondary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+                    <button className="btn btn-secondary mx-1 my-1" onClick={handleLowClick}>Convert to Lowercase</button>
+                    <button className="btn btn-secondary mx-1 my-1" onClick={handleEmailClick}>Purify Emails</button>
+                    <button className="btn btn-secondary mx-1 my-1" onClick={handleClear}>Clear Screen</button>
+                    <button id='copy' className="btn btn-secondary mx-1" onClick={handleCopy} >{btnText}</button>
+                </div>
+            </div>
+            <div className="container my-3" style={props.mode === 'dark' ? lightText : darkText} >
+                <h1 className='lead' style={props.mode === 'dark' ? lightText : darkText}>Your text summary</h1>
+                <p> <span className='display-6' style={props.mode === 'dark' ? lightText : darkText}>{text.split(' ').filter(word=>word!=='').length}</span> words, <span className='display-6'>{text.length}</span>characters</p>
+                <p >Average time to read the above text : {text.split(' ').filter(word=>word!=='').length / 125} minutes</p>
+                <h2 id='preview'>Preview:</h2>
+                <p>{text}</p>
+            </div>
+        </>
+    )
 }
+
