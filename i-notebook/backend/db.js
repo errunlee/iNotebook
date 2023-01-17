4cfa24a406d8529be2b79e08f8cfe17ee30d4e36
errunlee <32530613+errunlee@users.noreply.github.com>
2023-01-17 11:55:40 +0545

    some changes

--- i-notebook/backend/db.js
+++ i-notebook/backend/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const mongoURI='mongodb://localhost:27017'
+const mongoURI='mongodb+srv://errunlee:csidb977@cluster0.tcw7sei.mongodb.net/test'
 mongoose.set('strictQuery', false);
 const connectToMongo=async ()=>{
     mongoose.connect(mongoURI,()=>{
--- i-notebook/backend/index.js
+++ i-notebook/backend/index.js
@@ -5,10 +5,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+//routes
+app.use('/api/auth',require('./routes/auth'))
+// app.use('/api/notes',require('./routes/notes'))
